b5769d7f75315c668f07862c506a477f10ab8d22
hebatist <hebaja@yahoo.com.br>
2026-06-03 21:55:33 -0300

    fix: added const/non-const correctness

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -2,9 +2,17 @@
 
 #include <cstddef>
 
-template <typename T, typename Func>
+template <typename T>
 
-void	iter(T *arr, size_t length, Func f)
+void	iter(T *arr, const size_t length, void (*f)(T&))
+{
+	for (size_t i = 0; i < length; i++)
+		f(arr[i]);
+}
+
+template <typename T>
+
+void	iter(T *arr, const size_t length, void (*f)(const T&))
 {
 	for (size_t i = 0; i < length; i++)
 		f(arr[i]);
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,11 +1,6 @@
 #include "iter.hpp"
 #include <iostream>
 
-void	function(int &x)
-{
-	std::cout << x << std::endl;
-}
-
 class	Human
 {
 private:
@@ -27,27 +22,56 @@ public:
 	}
 };
 
-void	function_human(Human h)
+void	function_human(Human &h)
 {
 	std::cout << "Name -> " << h.getName() << " - Age -> " << h.getAge() << std::endl;
 }
 
+void	function_int(const int &x)
+{
+	std::cout << x << std::endl;
+}
+
+void	function_string(std::string &str)
+{
+	str.insert(0, "*** ");
+	str.insert(str.length(), " ***");
+}
+
 int	main()
 {
+	/* NON-CONST ARRAY */
 	{
 		size_t	size = 5;
-		int *arr = new int;
+		int arr[size];
 		for (size_t i = 0; i < size; i++)
 			arr[i] = i;
-
-		iter(arr, size -1, function);
-		delete arr;
+		iter(arr, size, function_int);
+	}
+	{
+		std::string arr[4] = {"C", "Java", "C++", "Kotlin"};
+		for (int i = 0; i < 4; i++)
+			std::cout << "String -> " << arr[i] << std::endl;
+		iter(arr, 4, function_string);		
+		for (int i = 0; i < 4; i++)
+			std::cout << "String -> " << arr[i] << std::endl;
 	}
 	{
 		Human arr[2] = {Human("Serumaninho", 6), Human("Caramelo", 5)};
-
 		iter(arr, 2, function_human);
 	}
-
+	/* SINGLE SIZE ARRAY */
+	{
+		size_t	size = 1;
+		int arr[size];
+		for (size_t i = 0; i < size; i++)
+			arr[i] = i;
+		iter(arr, size, function_int);
+	}
+	/* CONST ARRAY */
+	{
+		const int arr[7] = {0, 1, 2, 3, 4, 5, 6};
+		iter(arr, 7, function_int);
+	}
 	return (0);
 }
