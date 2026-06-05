14a599317908b0c0955065a76ede7df28f1c3878
Henrique <hebaja@yahoo.com.br>
2026-06-04 21:46:58 -0300

    ex01 lightly refactored

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,19 +1,15 @@
 #pragma once
 
-#include <cstddef>
-
 template <typename T>
-
-void	iter(T *arr, const size_t length, void (*f)(T&))
+void	iter(T *arr, const unsigned int length, void (*f)(T&))
 {
-	for (size_t i = 0; i < length; i++)
+	for (unsigned int i = 0; i < length; i++)
 		f(arr[i]);
 }
 
 template <typename T>
-
-void	iter(T *arr, const size_t length, void (*f)(const T&))
+void	iter(T *arr, const unsigned int length, void (*f)(const T&))
 {
-	for (size_t i = 0; i < length; i++)
+	for (unsigned int i = 0; i < length; i++)
 		f(arr[i]);
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -29,7 +29,7 @@ void	function_human(Human &h)
 
 void	function_int(const int &x)
 {
-	std::cout << x << std::endl;
+	std::cout << x << " ";
 }
 
 void	function_string(std::string &str)
@@ -42,13 +42,17 @@ int	main()
 {
 	/* NON-CONST ARRAY */
 	{
+		std::cout << "::: NON-CONST INT ARRAY :::" << std::endl;
 		size_t	size = 5;
 		int arr[size];
 		for (size_t i = 0; i < size; i++)
 			arr[i] = i;
 		iter(arr, size, function_int);
+		std::cout << std::endl;
 	}
 	{
+		std::cout << std::endl;
+		std::cout << "::: NON-CONST ARRAY ARRAY :::" << std::endl;
 		std::string arr[4] = {"C", "Java", "C++", "Kotlin"};
 		for (int i = 0; i < 4; i++)
 			std::cout << "String -> " << arr[i] << std::endl;
@@ -56,22 +60,20 @@ int	main()
 		for (int i = 0; i < 4; i++)
 			std::cout << "String -> " << arr[i] << std::endl;
 	}
+	/* CLASS */
 	{
+		std::cout << std::endl;
+		std::cout << "::: CLASS :::" << std::endl;
 		Human arr[2] = {Human("Serumaninho", 6), Human("Caramelo", 5)};
 		iter(arr, 2, function_human);
 	}
-	/* SINGLE SIZE ARRAY */
-	{
-		size_t	size = 1;
-		int arr[size];
-		for (size_t i = 0; i < size; i++)
-			arr[i] = i;
-		iter(arr, size, function_int);
-	}
 	/* CONST ARRAY */
 	{
+		std::cout << std::endl;
+		std::cout << "::: CONST ARRAY :::" << std::endl;
 		const int arr[7] = {0, 1, 2, 3, 4, 5, 6};
 		iter(arr, 7, function_int);
+		std::cout << std::endl;
 	}
 	return (0);
 }
